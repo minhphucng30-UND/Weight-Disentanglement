b151aeb21a596b56b3467a829da910b7942a312b
Phuc3010 <marksmans2002@gmail.com>
2026-04-02 05:05:30 +0200

    Add replay buffer

---
+++ replay_buffer.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import Any
+from verl import DataProto
+import torch
+
+class ReplayBuffer:
+    def __init__(self, max_size: int):
+        self.max_size = max_size
+        self.buffer = []
+    
+    def add_to_buffer(self, samples):
+        if len(self.buffer) == 0:
+            self.buffer = samples
+        else:
+            self.buffer = DataProto.concat([self.buffer, samples])
+
+    def get_from_buffer(self, count, dp_size):
+        if count > self.buffer_size():
+            count = (self.buffer_size() // dp_size) * dp_size
+        samples = self.buffer.slice(range(0, count))
+        self.buffer = self.buffer.slice(range(count, self.buffer_size()))
+        return samples
+
+    def buffer_size(self):
+        return len(self.buffer)
